1fa3e3ba43bf8f2f9708a53628731fee8409639d
smarr2198 <smarr2198@gmail.com>
2022-12-05 12:27:12 -0600

    FlappyBird Clone push

--- bird.js
+++ bird.js
@@ -1,6 +1,6 @@
 const birdElem = document.querySelector("[data-bird]");
-const BIRD_SPEED = 0.5;
-const JUMP_DURATION = 125;
+const BIRD_SPEED = 0.4;
+const JUMP_DURATION = 275;
 let timeSinceLastJump = Number.POSITIVE_INFINITY;
 
 export function setupBird() {
--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 import { updateBird, setupBird, getBirdRect } from "./bird.js";
+import {
+  updatePipes,
+  setupPipes,
+  getPassedPipesCount,
+  getPipeRects,
+} from "./pipe.js";
 
 document.addEventListener("keypress", handleStart, { once: true });
 const title = document.querySelector("[data-title]");
 const subtitle = document.querySelector("[data-subtitle]");
 
 let lastTime;
-
 function updateLoop(time) {
   if (lastTime == null) {
     lastTime = time;
@@ -14,6 +19,7 @@ function updateLoop(time) {
   }
   const delta = time - lastTime;
   updateBird(delta);
+  updatePipes(delta);
   if (checkLose()) return handleLose();
   lastTime = time;
   window.requestAnimationFrame(updateLoop);
@@ -21,15 +27,24 @@ function updateLoop(time) {
 
 function checkLose() {
   const birdRect = getBirdRect();
-
+  const insidePipe = getPipeRects().some((rect) => isCollision(birdRect, rect));
   const outsideWorld = birdRect.top < 0 || birdRect.bottom > window.innerHeight;
+  return outsideWorld || insidePipe;
+}
 
-  return outsideWorld;
+function isCollision(rect1, rect2) {
+  return (
+    rect1.left < rect2.right &&
+    rect1.top < rect2.bottom &&
+    rect1.right > rect2.left &&
+    rect1.bottom > rect2.top
+  );
 }
 
 function handleStart() {
   title.classList.add("hide");
   setupBird();
+  setupPipes();
   lastTime = null;
   window.requestAnimationFrame(updateLoop);
 }
@@ -38,7 +53,7 @@ function handleLose() {
   setTimeout(() => {
     title.classList.remove("hide");
     subtitle.classList.remove("hide");
-    subtitle.textContent = "0 Pipes";
+    subtitle.textContent = `${getPassedPipesCount()} Pipes`;
     document.addEventListener("keypress", handleStart, { once: true });
-  }, 300);
+  }, 350);
 }
